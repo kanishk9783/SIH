24c3dfb47dfb7367e31e2b5128300f2cb2ee1853
Himanshi9725 <hsuhalka25@gmail.com>
2023-09-21 22:39:52 +0530

    final commit

---
+++ Models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+const User = mongoose.model("User", schema);
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const User = require("./Models/user.model");
+const app = express();
+const mongoose = require("mongoose");
+
+try {
+  mongoose
+    .connect(
+      "mongodb+srv://audichya9876:gssVNvRtSRU6v2Us@cluster0.iqh4blf.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log("Connected to database!");
+    });
+} catch (error) {
+  console.log(error);
+}
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.send(user);
+  } catch (error) {
+    res.status(404).send("User not found");
+  }
+});
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send("User Not Found");
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+app.patch("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    res.send("User Details Updated");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+app.delete("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    res.status(200).send("Deleted");
+  } catch (error) {
+    res.status(500).send("User Not Found");
+  }
+});
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
